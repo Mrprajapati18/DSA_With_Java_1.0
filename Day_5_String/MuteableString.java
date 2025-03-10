afa691793162750b04c8372beac81997f7720297
MrPrajapati18 <durgeshkumar778080@gmail,com>
2025-03-10 23:33:41 +0530

    Mutable String

---
+++ Day_5_String/MuteableString.java
@@ -0,0 +1,9 @@
+public class MuteableString {
+  public static void main(String[] args) {
+    StringBuffer str = new StringBuffer("Kodnest");
+    System.out.println(str);
+    str.append("Technologies PVT LTD");
+    System.out.println(str);
+  }
+  
+}
